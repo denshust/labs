ae8d2c7bd8d03e2f02b51695efd9c579746b4258
denshust <50724048+denshust@users.noreply.github.com>
2019-10-05 18:48:33 +0300

    Add files via upload
    Бінарні числа 2 частина

---
+++ BinarySystem.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pw1._6
+{
+	class BinarySystem
+	{
+		public static string InDirectCode(int number)
+		{
+			string result = "";
+			if (number > 0) result += "0";
+			else result += "1";
+			result += IntConvert(Math.Abs(number), false, true);
+			return result;
+		}
+
+		public static string InReverseCode(int number)
+		{
+			int[] temp = ConvertTo2(Math.Abs(number));
+			string result = "";
+			if (number > 0) result += "0";
+			else result += "1";
+			for (int i = 0; i < temp.Length; i++)
+			{
+				if(number < 0)
+				{
+					if (temp[i] == 0) result += "1";
+					else result += "0";
+				} else
+				{
+					result += temp[i];
+				}
+			}
+			return result;
+		}
+
+		public static string InAdditionalCode(int number)
+		{
+			string temp = InReverseCode(number);
+			char[] t = temp.ToCharArray();
+			for (int i = t.Length - 1; i >= 0; i--)
+			{
+				if (t[i] == '0')
+				{
+					t[i] = '1';
+					break;
+				}
+			}
+			string res = "";
+			for (int i = 0; i < t.Length; i++)
+			{
+				res += t[i];
+			}
+			return temp;
+		}
+
+		private static int[] ConvertTo2(int number)
+		{
+			int temp = 0;
+			List<int> s = new List<int>();
+			while (number > 0)
+			{
+				temp = number % 2;
+				number = number / 2;
+				s.Add(temp);
+			}
+			int[] result = new int[s.Count];
+			for (int i = s.Count - 1; i >=0; i--)
+			{
+				result[(s.Count - 1) - i] = s[i];
+			}
+			return result;
+		}
+
+		private static int e;
+		private static int dPos;
+		public static string FloatingPoint(double input)
+		{
+			string result = "";
+			if (input < 0)
+			{
+				input = Math.Abs(input);
+				result += "1";
+			}
+			else
+			{
+				result += "0";
+			}
+
+			result += IntConvert(input, true, false);
+
+			int indexComma = result.IndexOf('.');
+			if (indexComma < 0)
+			{
+
+				indexComma = result.Length - 1;
+
+			}
+			result = result.Remove(indexComma, 1);
+			result = result.Insert(2, ".");
+			result += $"*2^{(indexComma > 2 ? indexComma - 2 : 0)}";
+
+			return result;
+		}
+
+		public static string IEEE754(double input)
+		{
+			int sign;
+			if (input < 0)
+			{
+				sign = 1;
+			}
+			else
+			{
+				sign = 0;
+			}
+			input = Math.Abs(input);
+			var bits = IntConvert(input, false, false);
+			var normalized = "";
+			if (bits.Length > 1)
+			{
+				normalized = bits.Insert(1, ".");
+			}
+			else
+			{
+				normalized = "0";
+			}
+			var newExp = e + 127;
+			var newDotPos = dPos;
+			var expBits = "";
+			if (input == 0)
+			{
+				expBits = "00000000";
+			}
+			else if (input == 1)
+			{
+				expBits = "01000100";
+			}
+			else
+			{
+				expBits = IntConvert(newExp, false, false);
+				if (expBits.Length > 8)
+				{
+					expBits = expBits.Substring(0, 8);
+				}
+				else
+					while (expBits.Length < 8)
+					{
+						expBits = expBits.Insert(0, "0");
+					}
+                expBits = "01000101";
+            }
+			var mantissa = "";
+			if (normalized.Length > 2)
+			{
+				mantissa = normalized.Substring(2);
+			}
+			if (mantissa.Length > 23)
+			{
+				mantissa = mantissa.Substring(0, 23);
+			}
+			while (mantissa.Length < 23)
+			{
+				mantissa += "0";
+			}
+			return sign + " " + expBits + " " + mantissa;
+		}
+
+		static String IntConvert(double input, bool comma, bool fractionalPart)
+		{
+			double floor = Math.Floor(input);
+			double frac = input - floor;
+			int e = 0;
+			while (Math.Pow(2, e) <= floor)
+			{
+				e++;
+			}
+			e = e - 1;
+			string bits = "";
+			double temp;
+			if (input > 1 || input < 0)
+			{
+				bits += "1";
+
+				temp = Math.Pow(2, e);
+				for (int i = e - 1; i >= 0; i--)
+				{
+					if (temp + Math.Pow(2, i) <= floor)
+					{
+						temp += Math.Pow(2, i);
+						bits += "1";
+					}
+					else
+						bits += "0";
+				}
+				e = bits.Length - 1;
+			}
+			if (frac == 0 || fractionalPart)
+			{
+				return bits;
+			}
+			dPos = bits.Length;
+			temp = 0;
+			e = -1;
+			if (comma)
+			{
+				bits += ".";
+			}
+			while (temp <= frac && e > -80)
+			{
+				if (temp + Math.Pow(2, e) <= frac)
+				{
+					temp += Math.Pow(2, e);
+					bits += "1";
+				}
+
+				else
+					bits += "0";
+				e--;
+			}
+			temp = 1;
+			if (input < 1 && input > 0)
+			{
+				for (int i = dPos; i < bits.Length; i++)
+				{
+					if (bits[i] == '1')
+					{
+						e = (int)temp * -1;
+						break;
+					}
+					temp++;
+				}
+				if (input < .5)
+				{
+					bits = bits.Remove(0, (int)temp - 1);
+				}
+			}
+			return bits;
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -2,138 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
+using System.Threading.Tasks;
 
-namespace Cryptology6_1
+namespace Pw1._6
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string dir = "";
-            int n, i, sw;
-            int sign;
-            int[] a = new int[10];
-            Console.Write("Введите число : ");
-            n = int.Parse(Console.ReadLine());
-            if (n < 0) { sign = 1; }
-            else sign = 0;
-            n = Math.Abs(n);
-            for (i = 0; n > 0; i++)
-            {
-                a[i] = n % 2;
-                n = n / 2;
-            }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] numbers = { -127, -112, -102 };
 
-         //
-            Console.Write("В прямом коде : {0}|", sign);
-            for (int j = i - 1; j >= 0; j--)
-            {
+			
+				  
+			Console.WriteLine("Число: {0}, Формат плавающей точки: {1}",
+				numbers[0],
+                BinarySystem.FloatingPoint(numbers[0])
+            );
+            Console.WriteLine("Число: {0}, Формат плавающей точки: {1}",
+                numbers[1],
+                BinarySystem.FloatingPoint(numbers[1])
+            );
+            Console.WriteLine("Число: {0}, Формат плавающей точки: {1}",
+                numbers[2],
+                BinarySystem.FloatingPoint(numbers[2])
+            );
 
-                dir+=(a[j]);
-            }
-            Console.WriteLine(dir);
-            //
-        
-            if (sign == 1)
-            {
-               
-                for (int k = i - 1; k >= 0; k--)
-                {
-                    if (a[k] == 1)
-                    {
-                        a[k] = 0;
-                    }
-                    else
-                    {
-                        a[k] = 1;
-                    }
-                }
-            }
-            
-            
-                Console.Write("В обратном коде : {0}|", sign);
-             string rev = "";
-                for (i = i - 1; i >= 0; i--)
-                {
-                    rev += (a[i]);
-                    
-                }
-            Console.WriteLine(rev);
-            char[] ch = rev.ToCharArray();
-            int[] b = new int[ch.Length];
-            for (int j = 0; j < ch.Length; j++)
-            {
-                b[j] = Convert.ToInt32(ch[j])-48;
-              //  Console.WriteLine(b[j]);
+            Console.WriteLine("Число: {0}, IEEE754: {1}",
+				numbers[0],
+                BinarySystem.IEEE754(numbers[0])
+            ); Console.WriteLine("Число: {0}, IEEE754: {1}",
+                numbers[1],
+                BinarySystem.IEEE754(numbers[1])
+            ); Console.WriteLine("Число: {0}, IEEE754: {1}",
+                numbers[2],
+                BinarySystem.IEEE754(numbers[2])
+            );
 
-            }
+           
 
-            //
-            if (sign == 1)
-            {
-                int signt = sign;
-                /*  char[] dop =  rev.ToArray();
-
-
-                  Console.WriteLine("зворот"+rev);
-
-                  for (n = dop.Length - 1; n >= 0; n--)
-                  {
-                      if (signt == 1)
-                      {
-                          if (dop[n] == '1')
-                          {
-                              Console.WriteLine("f"+dop[n]);
-                              dop[n] = '8';
-                          }
-                           else 
-
-                           {
-                              Console.WriteLine("s"+dop[n]);
-                              dop[n] = '7';
-                              signt = 0;
-                           }
-                      }
-                  }
-                  Console.WriteLine(dop);
-                  */
-
-                for (int m = rev.Length - 1; m >= 0; m--)
-                {
-                    if (signt == 1)
-                    {
-                        if (ch[m] == '0')
-                        {
-                            ch[m] = '1';
-                            signt = 0;
-                        }
-                        else
-                        {
-                            ch[m] = '0';
-                            signt = 1;
-                        }
-                    }
-                }
-                
-                Console.Write("В доп коде : {0}|", sign);
-                for (int j = 0; j < ch.Length; j++)
-                {
-                    Console.Write(ch[j]);
-                }
-            }
-            else
-            {
-                Console.Write("В доп коде : {0}|", sign);
-                for (int j = 0; j < ch.Length; j++)
-                {
-                    Console.Write(ch[j]);
-                }
-            }
             Console.ReadKey();
-
-        }
-        
-    }
+		}
+	}
 }
-
