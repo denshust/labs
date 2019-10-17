133c5d55f2e4f565e6d400e5733e6714793cb899
denshust <50724048+denshust@users.noreply.github.com>
2019-10-17 04:08:32 +0300

    Add files via upload

---
+++ ProgramLoz.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1_Лозунговый
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Лозунг: ");
+            var slogan = Console.ReadLine();
+
+            var cipher = new SloganCipher(slogan);
+
+            Console.Write("Текст для шифрования: ");
+            var text = Console.ReadLine();
+
+            var cipheredText = cipher.Cipher(text);
+            Console.WriteLine("Зашифрованный текст: {0}", cipheredText);
+
+            var decipheredText = cipher.Decipher(cipheredText);
+            Console.WriteLine("Расшифрованный текст: {0}", decipheredText);
+
+            Console.ReadKey();
+        }
+    }
+}
